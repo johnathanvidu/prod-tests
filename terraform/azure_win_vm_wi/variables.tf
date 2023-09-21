e9298d65e6fa6c83bda810c46eb1885d79438d37
johnathanvidu <johnathan.v@qualisystems.com>
2023-09-21 15:12:27 +0300

    Create variables.tf

---
+++ terraform/azure_win_vm_wi/variables.tf
@@ -0,0 +1,11 @@
+variable "vm_name" {
+  type = string
+  default = "vido_vm_win"
+  description = "VM Name"
+}
+
+variable "resource_group" {
+  type = string
+  default = "vido_rg_win"
+  description = "resource group name"
+}
