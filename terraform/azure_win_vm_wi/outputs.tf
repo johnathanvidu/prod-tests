beb1296d37ac4eaa2ad1bb1b354ac872d12d77ca
johnathanvidu <johnathan.v@qualisystems.com>
2023-09-21 15:11:53 +0300

    Create outputs.tf

---
+++ terraform/azure_win_vm_wi/outputs.tf
@@ -0,0 +1,7 @@
+output "public_ip" {
+  value = azurerm_windows_virtual_machine.example.network_interface_ids[0]
+}
+
+output "password" {
+  value = azurerm_windows_virtual_machine.example.admin_password
+}
