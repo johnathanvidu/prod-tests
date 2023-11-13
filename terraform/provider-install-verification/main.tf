853ba07f353fbef2dc56907833364aa8be5f8dcf
DBS <96681520+davidbquali@users.noreply.github.com>
2023-11-14 00:56:40 +0200

    Update main.tf

--- terraform/provider-install-verification/main.tf
+++ terraform/provider-install-verification/main.tf
@@ -11,11 +11,11 @@ provider "torque" {}
 resource "torque_introspection_resource" "example" {
     display_name = "Vido Resource"
     image = "https://cdn-icons-png.flaticon.com/512/882/882730.png"
-    introspection_data = {size = "xtra large", mode = "party"}
+    introspection_data = {kind = "vido resource", ip = "192.168.0.22", pwd = "*****"}
 }
 
 resource "torque_introspection_resource" "example2" {
     display_name = "Vido Resource"
     image = "https://www.svgrepo.com/show/533037/balloon.svg"
-    introspection_data = {size = "xtra large", mode = "party"}
+    introspection_data = {size = "xlarge", mode = "party"}
 }
