a4d7aa052bc1a4c8e23d0ab3c0ea9cf395d04e0b
johnathanvidu <johnathan.v@qualisystems.com>
2024-09-19 22:39:18 +0300

    multiple regions

--- scripts/bulk-deploy/main.py
+++ scripts/bulk-deploy/main.py
@@ -1,73 +1,74 @@
-import requests
 import sys
 import uuid
+import requests
 
 
 def make_post_request(url, data, headers):
     response = requests.post(url, json=data, headers=headers)
     return response.status_code, response.json()
 
-
-def main():
-    if len(sys.argv) != 6:
-        print("Usage: python3 main.py <token> <region1> <region2> <space> <application>")
-        print("Example: python3 main.py 2 ML dev")
-        sys.exit(1)
-
-    token = sys.argv[1]
-    region1 = sys.argv[2]
-    region2 = sys.argv[3]
-    space = sys.argv[4]
-    application = sys.argv[5]
-
-    if application == "prod":
-        blueprint_name = "hello-world-terragrunt" 
-    elif application == "qa":
-        blueprint_name = "hello-world-terragrunt"
-    elif application == "dev":
-        blueprint_name = "hello-world-terragrunt"
-    else:
-        print("Unknown application type. Please use 'prod', 'qa', or 'dev'.")
-        sys.exit(1)
-
+def launch_env(token, space, blueprint_name, region, duration):
     url = f"https://preview.qualilabs.net/api/spaces/{space}/environments"
 
     payload = {
-        "environment_name": f"processing cluster {str(uuid.uuid4())}",
+        "environment_name": f"processing cluster {region} {str(uuid.uuid4())}",
         "blueprint_name": blueprint_name,
-        "owner_email": "david.b@quali.com",
+        "owner_email": "johnathan.v@quali.com", # change to the correct owner
         "source": {
-            "blueprint_name": blueprint_name,
-            "repository_name": "bps",
+            "repository_name": "bps"
         },
         "inputs": {
-            "agent": "demo-prod",
-            "1st-region": "us-east-1",
-            "2nd-region": "us-east-1"
+            "agent": "demo-prod", # change to the correct agent
         },
         "labels": [
             {
                 "key": "region",
-                "value": "us-east-1"
+                "value": f"{region}"
             }
         ],
-        # "collaborators": {
-        #     "collaborators_emails": ["string"],
-        #     "all_space_members": True
-        # },
         "automation": True,
-        # "scheduled_end_time": "2019-08-24T14:15:22Z",
-        "duration": "PT1H"
+        "duration": f"PT{duration}H"
     }
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
         "Authorization": f"Bearer {token}"
     }
+    return make_post_request(url, payload, headers)
 
-    status_code, response_data = make_post_request(url, payload, headers)
-    print(
-        f"Status Code: {status_code}, Response: {response_data}, Headers: {headers}")
+def main():
+    if len(sys.argv) != 7:
+        print("Usage: python3 main.py <token> <region1> <region2> <region3> <region4> <space> <duration>")
+        print("Example: python3 main.py 2 ML dev")
+        sys.exit(1)
+
+    token = sys.argv[1]
+    region1 = sys.argv[2]
+    region2 = sys.argv[3]
+    region3 = sys.argv[4]
+    region4 = sys.argv[5]
+    space = sys.argv[6]
+    duration = sys.argv[7]
+
+    region_to_bp = {
+        'eu-west-1': 'hello-world-terragrunt',
+        'eu-west-2': 'hello-world-terragrunt',
+        'us-east-1': 'hello-world-terragrunt',
+        'us-east-2': 'hello-world-terragrunt'
+    }
+
+    blueprints = []
+    blueprints.append(region_to_bp[region1])
+    if region2 != 'pass':
+        blueprints.append(region_to_bp[region2])
+    if region3 != 'pass':
+        blueprints.append(region_to_bp[region3])
+    if region4 != 'pass':
+        blueprints.append(region_to_bp[region4])
+
+    for blueprint_name in blueprints:
+        status_code, response_data = launch_env(token, space, blueprint_name, duration)
+
 
     # number_of_requests = 1 if region2 == "pass" else 2
     # for _ in range(number_of_requests):
