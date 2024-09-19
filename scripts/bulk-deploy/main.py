93147d0bb24ac3b7f37511fdf2c7c6cea075836f
johnathanvidu <johnathan.v@qualisystems.com>
2024-09-20 01:01:34 +0300

    recent

--- scripts/bulk-deploy/main.py
+++ scripts/bulk-deploy/main.py
@@ -8,17 +8,17 @@ def make_post_request(url, data, headers):
     return response.status_code, response.json()
 
 def launch_env(token, space, blueprint_name, region, duration):
-    url = f"https://preview.qualilabs.net/api/spaces/{space}/environments"
+    url = f"https://portal.qtorque.io/api/spaces/{space}/environments"
 
     payload = {
         "environment_name": f"processing cluster {region} {str(uuid.uuid4())}",
         "blueprint_name": blueprint_name,
-        "owner_email": "johnathan.v@quali.com", # change to the correct owner
+        "owner_email": "johnathan.v@quali.com",
         "source": {
             "repository_name": "bps"
         },
         "inputs": {
-            "agent": "demo-prod", # change to the correct agent
+            "agent": "vido-vido-prod",
         },
         "labels": [
             {
@@ -37,34 +37,25 @@ def launch_env(token, space, blueprint_name, region, duration):
     return make_post_request(url, payload, headers)
 
 def main():
-    if len(sys.argv) != 8:
-        print("Usage: python3 main.py <token> <region1> <region2> <region3> <region4> <space> <duration>")
-        print("Example: python3 main.py 2 ML dev")
+    if len(sys.argv) != 6:
+        print("Usage: python3 main.py <token> <region1> <region2> <space> <duration>")
         sys.exit(1)
 
     token = sys.argv[1]
     region1 = sys.argv[2]
     region2 = sys.argv[3]
-    region3 = sys.argv[4]
-    region4 = sys.argv[5]
-    space = sys.argv[6]
-    duration = sys.argv[7]
+    space = sys.argv[4]
+    duration = sys.argv[5]
 
-    region_to_bp = {
-        'eu-west-1': 'hello-world-terragrunt',
-        'eu-west-2': 'hello-world-terragrunt',
-        'us-east-1': 'hello-world-terragrunt',
-        'us-east-2': 'hello-world-terragrunt'
+    region_to_bp = {  # change those to the right blueprint names
+        'eu-west-1': 'hello-world-terragrunt', 
+        'us-east-1': 'hello-pre-scripts',
     }
 
     blueprints = []
     blueprints.append((region1, region_to_bp[region1]))
     if region2 != 'pass':
         blueprints.append((region2, region_to_bp[region2]))
-    if region3 != 'pass':
-        blueprints.append((region3, region_to_bp[region3]))
-    if region4 != 'pass':
-        blueprints.append((region4, region_to_bp[region4]))
 
     for region, blueprint_name in blueprints:
         status_code, response_data = launch_env(token, space, blueprint_name, region, duration)
@@ -72,14 +63,7 @@ def main():
             exit(1)
         env_id = response_data.get('id')
         env_var_name = region.replace('-', '_')
-        print(f'export {env_var_name}=https://preview.qualilabs.net/{space}/environments/{env_id}')
-
-
-    # number_of_requests = 1 if region2 == "pass" else 2
-    # for _ in range(number_of_requests):
-    #     status_code, response_data = make_post_request(url, payload, headers)
-    #     print(
-    #         f"Status Code: {status_code}, Response: {response_data}, Headers: {headers}")
+        print(f'export {env_var_name}=https://portal.qtorque.io/{space}/environments/{env_id}')
 
 
 if __name__ == "__main__":
