f15ac91a3951b050138f60a44762f197fce5d946
johnathanvidu <johnathan.v@qualisystems.com>
2024-11-05 14:57:07 +0200

    Create main.tf

---
+++ terraform/assume-remote-role/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  assume_role {
+    role_arn = "arn:aws:iam::245147192452:role/vido-role"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
