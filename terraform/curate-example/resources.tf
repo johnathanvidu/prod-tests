8927e16877762cb34fdf89a5ee0aa22cd27bf6eb
johnathanvidu <johnathan.v@qualisystems.com>
2024-11-11 21:47:22 +0200

    Update resources.tf

--- terraform/curate-example/resources.tf
+++ terraform/curate-example/resources.tf
@@ -1,6 +1,6 @@
 
 resource "aws_s3_bucket" "bucket_axgh30c" {
-  provider            = aws.us_east_1
+  provider            = aws.eu_west_1
   bucket              = "tf-backend-terragrunt2"
   bucket_prefix       = null
   force_destroy       = null
