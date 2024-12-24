8b85ee9713c755ea23dd8e1b6473484d3a2ecacd
johnathanvidu <johnathan.v@qualisystems.com>
2024-12-24 20:11:32 +0200

    Update staterm.sh

--- scripts/staterm.sh
+++ scripts/staterm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo $TORQUE_TF_EXECUTABLE
+echo $TORQUE_TF_MODULE_PATH
+
 state=$TORQUE_TF_MODULE_PATH/terraform.tfstate
 
 $TORQUE_TF_EXECUTABLE state rm 'aws_s3_bucket.bucket' -state=$state
