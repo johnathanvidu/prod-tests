25268ed3ca5a5403c24e3fa44452f57b5e85cfa8
johnathanvidu <johnathan.v@qualisystems.com>
2024-12-24 20:27:27 +0200

    Update staterm.sh

--- scripts/staterm.sh
+++ scripts/staterm.sh
@@ -5,6 +5,7 @@ echo $TORQUE_TF_MODULE_PATH
 
 state=$TORQUE_TF_MODULE_PATH/terraform.tfstate
 
+echo 'removing aws_s3_bucket.bucket from the state file'
 $TORQUE_TF_EXECUTABLE state rm -state='$state' 'aws_s3_bucket.bucket'
 
 $TORQUE_TF_EXECUTABLE apply -state='$state' -refresh-only
