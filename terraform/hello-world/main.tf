cddff8bfbf0c17e1db5cc043db2baa13e2366d8a
johnathanvidu <johnathan.v@qualisystems.com>
2025-02-18 20:37:18 +0200

    Update main.tf

--- terraform/hello-world/main.tf
+++ terraform/hello-world/main.tf
@@ -12,5 +12,5 @@ variable "vido2" {
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
-  value = "Hello, World!"
+  value = "Hello, World! ${var.vido2}"
 }
