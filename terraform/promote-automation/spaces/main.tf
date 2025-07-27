9b3030407f775818c5b35b75b5ce85afb266cb6f
johnathanvidu <johnathan.v@qualisystems.com>
2025-07-27 16:51:22 +0300

    fix space name creation

--- terraform/promote-automation/spaces/main.tf
+++ terraform/promote-automation/spaces/main.tf
@@ -1,6 +1,6 @@
 resource "torque_space" "new_space" {
     count = 3
-    space_name = element(var.name, count.index)
+    space_name = "${var.name}-${count.index}"
     color      = var.color
     icon       = var.icon
 }
