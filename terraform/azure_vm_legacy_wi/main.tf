e51892e2a79dc99076bdaef0a486322e37bcaa87
johnathanvidu <johnathan.v@qualisystems.com>
2025-09-02 00:15:42 +0300

    Update main.tf

--- terraform/azure_vm_legacy_wi/main.tf
+++ terraform/azure_vm_legacy_wi/main.tf
@@ -14,6 +14,8 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
+
+  use_oidc = true
 }
 
 ## <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
